c579de2e0179babb8e0e1cfe93de0cb88347bb4f
jung <asas6306@naver.com>
2021-02-09 12:41:23 +0900

    게시물 추가 수정

--- src/main/java/com/example/demo/controller/ArticleController.java
+++ src/main/java/com/example/demo/controller/ArticleController.java
@@ -3,12 +3,15 @@ package com.example.demo.controller;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.dto.Article;
+import com.example.demo.dto.Member;
 import com.example.demo.service.ArticleService;
 import com.example.demo.util.ResultData;
 
@@ -26,20 +29,27 @@ public class ArticleController {
 				keyword = null;
 			}
 		}
-		
+
 		return as.getArticles(type, keyword);
 	}
 
 	@RequestMapping("/usr/article/detail")
 	@ResponseBody
 	public Article detail(int aid) {
-		
+
 		return as.getArticleById(aid);
 	}
 
 	@RequestMapping("/usr/article/add")
 	@ResponseBody
-	public ResultData add(Map<String, Object> param) {
+	public ResultData add(Map<String, Object> param, HttpSession session) {
+
+		if (session.getAttribute("m") == null) {
+			return new ResultData("F-2", "로그인이 필요한 기능입니다.");
+		} else {
+			Member m = (Member) session.getAttribute("m");
+			param.put("uid", m.getUid());
+		}
 		if (param.get("title") == null) {
 			return new ResultData("F-1", "제목을 입력해주세요.");
 		}
@@ -53,20 +63,20 @@ public class ArticleController {
 	@RequestMapping("/usr/article/update")
 	@ResponseBody
 	public ResultData update(Integer aid, String title, String body) {
-		if(aid == null) {
+		if (aid == null) {
 			return new ResultData("F-1", "게시물 id를 입력해주세요");
 		}
-		
+
 		return as.update(aid, title, body);
 	}
 
 	@RequestMapping("/usr/article/delete")
 	@ResponseBody
 	public ResultData delete(Integer aid) {
-		if(aid == null) {
+		if (aid == null) {
 			return new ResultData("F-1", "게시물 id를 입력해주세요");
 		}
-		
+
 		return as.delete(aid);
 	}
 
