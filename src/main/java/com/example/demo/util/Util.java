4ed0153d76fa40095e3a919870b905fb69b4684b
jung <asas6306@naver.com>
2021-02-15 18:05:18 +0900

    짜잘한 수정

--- src/main/java/com/example/demo/util/Util.java
+++ src/main/java/com/example/demo/util/Util.java
@@ -9,7 +9,7 @@ import java.util.Map;
 public class Util {
 	public static String getDate() {
 		Date date = new Date();
-		SimpleDateFormat sdf = new SimpleDateFormat("YYYY.MM.DD. hh:mm:ss");
+		SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-DD hh:mm:ss");
 
 		return sdf.format(date);
 	}
@@ -17,33 +17,33 @@ public class Util {
 	public static Map<String, Object> indexOf(Object... args) {
 		Map<String, Object> map = new LinkedHashMap<>();
 		int length = args.length;
-		
-		if(length%2 != 0) {
+
+		if (length % 2 != 0) {
 			throw new IllegalArgumentException("인자의 개수를 짝수로 입력해주세요.");
 		}
-		
-		for(int i =0; i<length; i+=2) {
-			map.put(String.valueOf(args[i]), args[i+1]);
+
+		for (int i = 0; i < length; i += 2) {
+			map.put(String.valueOf(args[i]), args[i + 1]);
 		}
-		
+
 		return map;
 	}
-	
+
 	public static int getAsInt(Object object, int defaultValue) {
 		if (object instanceof BigInteger) {
 			return ((BigInteger) object).intValue();
 		} else if (object instanceof Double) {
-			return (int) Math.floor((double)object);
+			return (int) Math.floor((double) object);
 		} else if (object instanceof Float) {
-			return (int) Math.floor((Float)object);
-		}else if (object instanceof Long) {
+			return (int) Math.floor((Float) object);
+		} else if (object instanceof Long) {
 			return (int) object;
 		} else if (object instanceof Integer) {
 			return (int) object;
 		} else if (object instanceof String) {
 			return Integer.parseInt((String) object);
 		}
-		
+
 		return defaultValue;
 	}
 }
