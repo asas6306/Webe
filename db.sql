3fe7734fd15cd9faf706b859d963290ac53d478f
jung <asas6306@naver.com>
2021-02-20 14:03:19 +0900

    db.sql boardtag 추가

--- db.sql
+++ db.sql
@@ -3,3 +3,13 @@
 select concat('제목_', floor(rand()*1000)+1), CONCAT('내용_', FLOOR(RAND()*1000)+1), 1, 0 from article;
 
 select count(*) from article; */
+
+
+CREATE TABLE boardtag (
+bid INT(5) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+tag INT(3) UNSIGNED UNIQUE,
+tagname CHAR(20) NOT NULL
+);
+
+INSERT INTO boardtag SET tag=1, tagname='공지사항';
+INSERT INTO boardtag SET tag=2, tagname='자유게시판';
