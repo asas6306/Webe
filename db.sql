78b5f38964e1f74a29f016296a1a747e6def72e8
jung <asas6306@naver.com>
2021-02-20 13:22:53 +0900

    테스트 데이터 생성

---
+++ db.sql
@@ -0,0 +1,5 @@
+/* 테스트 데이터 생성 */
+/* insert into article (title, body, uid, tag) 
+select concat('제목_', floor(rand()*1000)+1), CONCAT('내용_', FLOOR(RAND()*1000)+1), 1, 0 from article;
+
+select count(*) from article; */
